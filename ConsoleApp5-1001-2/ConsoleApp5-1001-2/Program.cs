9893294461ba0ad5ca9476413e8fddbb0ee5bb39
yapeng hao <yapeng.hao@email.com>
2019-10-01 15:11:24 -0400

    edit

--- ConsoleApp5-1001-2/ConsoleApp5-1001-2/Program.cs
+++ ConsoleApp5-1001-2/ConsoleApp5-1001-2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp5_1001_2
         static void Main(string[] args)
         {
             Console.WriteLine("dev");
+            Console.WriteLine("dev 2");
         }
     }
 }
